1c5c804113718cc3984f4dbec340724eb2f1e60f
Willian Dalarosa <newilliann@hotmail.com>
2019-12-20 01:19:17 -0300

    Criado metodo findById

--- src/main/java/com/willian/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/willian/socialbooks/resources/LivrosResources.java
@@ -1,8 +1,10 @@
 package com.willian.socialbooks.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,17 +16,23 @@ import com.willian.socialbooks.repository.LivrosRepository;
 @RestController
 @RequestMapping("/livros")
 public class LivrosResources {
-	
+
 	@Autowired
 	private LivrosRepository livrosRepository;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Livro> listar() {
-				return livrosRepository.findAll();
+	public List<Livro> findAll() {
+		return livrosRepository.findAll();
 	}
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Optional<Livro> findById(@PathVariable("id") Long id) {
+		return livrosRepository.findById(id);
+	}
+
 	@RequestMapping(method = RequestMethod.POST)
-	public void salvar(@RequestBody Livro livro) {
+	public void save(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
 	}
+
 }
